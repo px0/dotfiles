7bfacf7f67d0fe1323a6a09c61a97220c4185379
Maximilian Gerlach <m@px0.de>
2013-11-20 18:14:51 -0500

    improvements

--- extras/os_x_defaults.sh
+++ extras/os_x_defaults.sh
@@ -20,6 +20,27 @@ echo "Speed up dock show/hide animation."
 defaults write com.apple.dock autohide-time-modifier -float 0.5
 killall Dock
 
+echo "Use AirDrop over every interface. srsly this should be a default."
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
+
+echo "Always open everything in Finder's list view. This is important."
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+echo "Disable the sound effects on boot"
+sudo nvram SystemAudioVolume=" "
+
+echo "Always show scrollbars"
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+
+echo "Increase window resize speed for Cocoa applications"
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+echo "Disable opening and closing window animations"
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
+
+echo "Automatically quit printer app once the print jobs complete"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
 # http://osxdaily.com/2011/12/12/hide-spotlight-menu-icon-mac-os-x-lion/
 # echo "Hide Spotlight icon."
 # sudo chmod 600 /System/Library/CoreServices/Search.bundle/Contents/MacOS/Search
@@ -30,6 +51,32 @@ killall Dock
 # mkdir -p ~/Desktop/Screenshots
 # defaults write com.apple.screencapture location ~/Desktop/Screenshots/
 # killall SystemUIServer
+
+echo "Finder: disable window animations and Get Info animations"
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+echo "Finder: show hidden files by default"
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+echo "Finder: show all filename extensions"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+
+echo "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+###############################################################################
+# Trackpad, mouse, keyboard, Bluetooth accessories, and input #
+###############################################################################
+
+echo "Trackpad: enable tap to click for this user and for the login screen"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+
+echo "Disable “natural” (Lion-style) scrolling"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
 echo "Set a blazingly fast keyboard repeat rate."
 defaults write NSGlobalDomain KeyRepeat -int 0.02
@@ -75,6 +122,14 @@ defaults write com.apple.iTunes disablePingSidebar -bool true
 
 echo "Disable all the other Ping stuff in iTunes."
 defaults write com.apple.iTunes disablePing -bool true
+
+###############################################################################
+# Mail #
+###############################################################################
+
+echo "Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app"
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+
 
 echo "Changed defaults. Restarting apps…"
 
